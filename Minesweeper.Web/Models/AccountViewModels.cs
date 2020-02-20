75d3003d75abac01330c856072f6e88a91ed05c5
JD Kaim <jdkaim@hotmail.com>
2020-02-19 20:54:11 -0800

    Added login required to play.

--- Minesweeper.Web/Models/AccountViewModels.cs
+++ Minesweeper.Web/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace Minesweeper.Web.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,11 @@ namespace Minesweeper.Web.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [StringLength(3, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "3-Character Username")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
